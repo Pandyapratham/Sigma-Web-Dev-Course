21df80077bab20dce37a11e864d40b8a5d9e12ec
Pandya Pratham <130195859+Pandyapratham@users.noreply.github.com>
2024-07-15 14:53:30 +0530

    Update index.js

--- Video 57/index.js
+++ Video 57/index.js
@@ -1,33 +1,33 @@
 console.log("I am a tutorial on Loops")
 
 let a = 1;
-// console.log(a)
-// console.log(a+1)
-// console.log(a+2)
+     name: "Harry",
+     role: "Programmer",
+     company: "CodeWithHarry AI"
+} console.log(a)
+console.log(a+1)
+console.log(a+2)
 
-// for (let i = 0; i < 100; i++) { 
-//     console.log(a + i); 
-// }
+for (let i = 0; i < 100; i++) { 
+    console.log(a + i); 
+}
 
-// let obj = {
-//     name: "Harry",
-//     role: "Programmer",
-//     company: "CodeWithHarry AI"
-// }
+let obj = {
+
  
-// for (const key in obj) {  
-//         console.log(key)
-// }
+for (const key in obj) {  
+        console.log(key)
+}
 
-// for (const c of "Harry") {
-//     console.log(c)
-// }
+for (const c of "Harry") {
+    console.log(c)
+}
 
-// let i = 0;
-// while (i<60000) {
-//     console.log(i)
-//     i++;
-// }
+let i = 0;
+while (i<60000) {
+    console.log(i)
+    i++;
+}
 
 let i = 10;
 do {
